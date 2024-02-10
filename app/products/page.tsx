ca4f7d00a5e62519cc705109f953205ff45b6e36
Terrystan Sustal <terrysustal@gmail.com>
2024-02-10 12:55:55 -0800

    Added a grid for Product Page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,10 +7,10 @@ export default async function ProductsPage() {
   return (
     <>
       <div>
-        <h1>Products</h1>
+        <h1 className="text-4xl font-bold text-Kilamanjaro-950">Products</h1>
         <p>Products page content</p>
       </div>
-      <div className='flex'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
         {products.map((product) => {
           return (
             <div key={product.id} className='border-4 p-2 m-2'>
